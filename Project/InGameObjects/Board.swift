e8f36d7ef42f287bd2013e29aec6fd201a791389
Alexey Stepanov <st3p-by-step@ya.ru>
2023-01-12 21:27:38 +0300

    Basic versions of the Board struct and mapView inside ViewController are implemented

--- Project/InGameObjects/Board.swift
+++ Project/InGameObjects/Board.swift
@@ -4,5 +4,39 @@
 
 import Foundation
 
+enum BoardTile: Codable {
+    case empty
+    case room([Direction: Interaction])
+}
+
 struct Board: Codable {
+    private let width: Int
+    private let height: Int
+    private var field: [BoardTile]
+
+    public init() {
+        width = 0
+        height = 0
+        field = []
+    }
+
+    public init(JSON: String) {
+        let decoder = JSONDecoder()
+        let data = JSON.data(using: .utf8)!
+        self = try! decoder.decode(Board.self, from: data)
+    }
+
+    public func getWidth() -> Int { width }
+
+    public func getHeight() -> Int { height }
+
+    public func getTile(_ x: size_t, _ y: size_t) -> BoardTile { field[width * y + x] }
+
+    public func getTile(_ num: size_t) -> BoardTile { field[num] }
+
+    public func getJSON() -> String {
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(self)
+        return String(data: data, encoding: .utf8)!
+    }
 }
--- Project/ViewControllers/ViewController.swift
+++ Project/ViewControllers/ViewController.swift
@@ -1,51 +1,87 @@
 //
-//  ViewController.swift
-//  Project
-//
-//  Created by Алексей Степанов on 2023-01-07.
+// Created by Алексей Степанов on 2023-01-11.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    private var board: Board? = nil
+    let mapView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
-        // let board = Board()
-        // print(board.toStr())
-
-        var str = """
-                  {
-                  "width" : 2,
-                  "field" : [
-                  {
-                  "empty" : {
-
-                  }
-                  },
-                  {
-                  "empty" : {
-
-                  }
-                  },
-                  {
-                  "empty" : {
-
-                  }
-                  },
-                  {
-                  "empty" : {
-
-                  }
-                  }
-                  ],
-                  "height" : 2
-                  }
+        let str = """
+                  {"width":2,"field":[{"empty":{}},{"room":{"_0":[0,{}]}},{"room":{"_0":[0,{}]}},{"empty":{}}],"height":2}
                   """
-        let board2 = Board(JSON: str)
-        print(board2.toStr())
+        board = Board(JSON: str)
+        
+        mapView.isHidden = true
+        view.addSubview(mapView)
+        setupView()
+        setupMapView()
+    }
+
+    /*override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        setupView()
+    }*/
+
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+
+    private func setupUI() {
+
+    }
+
+    private func setupMapView() {
+        mapView.pinTop(to: view.safeAreaLayoutGuide.topAnchor)
+        mapView.pin(to: view, [(.left, 15), (.right, -15)])
+        mapView.pinBottom(to: view.safeAreaLayoutGuide.bottomAnchor)
+
+        if let board = board {
+            var prevTile: UIView? = nil
+            for i in 0..<board.getWidth() * board.getHeight() {
+                print(i)
+
+                let tile = UIView()
+                mapView.addSubview(tile)
+
+                switch board.getTile(i) {
+                case .empty:
+                    tile.backgroundColor = .systemYellow
+                case .room(_):
+                    tile.backgroundColor = .systemPurple
+                }
+
+                if i != 0 {
+                    if i % board.getWidth() == 0 {
+                        tile.pinTop(to: prevTile!.bottomAnchor)
+                        tile.pinLeft(to: mapView)
+                    } else {
+                        tile.pinTop(to: prevTile!)
+                        tile.pinLeft(to: prevTile!.trailingAnchor)
+                    }
+                } else {
+                    tile.pinTop(to: mapView)
+                    tile.pinLeft(to: mapView)
+                }
+
+                tile.pinHeight(to: mapView, 1 / CGFloat(board.getHeight()))
+                tile.pinWidth(to: mapView, 1 / CGFloat(board.getWidth()))
+
+                prevTile = tile
+            }
+        }
+    }
+
+    public func updateMap(_ board: Board) {
+        self.board = board
+        view.subviews.forEach { $0.removeFromSuperview() }
+
+        setupMapView()
     }
 }
-
